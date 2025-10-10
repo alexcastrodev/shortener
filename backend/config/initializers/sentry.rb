185762b14f9ffdd68076ec27ff8dadc064de008f
Alexandro castro <alexandrocastro@mac.home>
2025-10-10 11:30:23 +0100

    chore: use sentry only production

--- backend/config/initializers/sentry.rb
+++ backend/config/initializers/sentry.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  config.breadcrumbs_logger = [:active_support_logger]
-  config.dsn = ENV["SENTRY_DSN"]
-  config.profiles_sample_rate = 0.7
-  # https://docs.sentry.io/platforms/ruby/configuration/options/#tracing-options
-  config.traces_sample_rate = 0.7
+if Rails.env.production?
+  Sentry.init do |config|
+    config.breadcrumbs_logger = [:active_support_logger]
+    config.dsn = ENV["SENTRY_DSN"]
+    config.profiles_sample_rate = 0.7
+    # https://docs.sentry.io/platforms/ruby/configuration/options/#tracing-options
+    config.traces_sample_rate = 0.7
+  end
 end
