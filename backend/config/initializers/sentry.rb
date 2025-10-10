3fe28c1ad6798d03776871d560a2582a543f082f
Alexandro castro <alexandrocastro@mac.home>
2025-10-10 11:27:47 +0000

    chore: enable profiling

--- backend/config/initializers/sentry.rb
+++ backend/config/initializers/sentry.rb
@@ -4,9 +4,9 @@ if Rails.env.production? && ENV["SENTRY_DSN"].present?
   Sentry.init do |config|
     config.breadcrumbs_logger = [:active_support_logger]
     config.dsn = ENV["SENTRY_DSN"]
-    config.profiles_sample_rate = 0.7
+    config.profiles_sample_rate = 1.0
     # https://docs.sentry.io/platforms/ruby/configuration/options/#tracing-options
-    config.traces_sample_rate = 0.7
+    config.traces_sample_rate = 1.0
     config.enable_logs = true
     config.enabled_patches = [:logger]
   end
