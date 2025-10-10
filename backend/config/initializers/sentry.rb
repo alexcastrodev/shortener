b2f7c9155b782408d31657ebdc3cac9fe1dac054
Alexandro castro <alexandrocastro@mac.home>
2025-10-10 11:39:05 +0000

    remove logger from workers

--- backend/config/initializers/sentry.rb
+++ backend/config/initializers/sentry.rb
@@ -7,7 +7,7 @@ if Rails.env.production? && ENV["SENTRY_DSN"].present?
     config.profiles_sample_rate = 1.0
     # https://docs.sentry.io/platforms/ruby/configuration/options/#tracing-options
     config.traces_sample_rate = 1.0
-    config.enable_logs = true
+    config.enable_logs = ENV["SENTRY_LOGGER"].present?
     config.enabled_patches = [:logger]
   end
 end
