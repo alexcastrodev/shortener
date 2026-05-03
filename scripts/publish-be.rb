8a9dc3e146a0409af9444d9499584a041cf30497
alexcastrodev <alexoliveira7x@gmail.com>
2026-05-03 12:24:39 +0100

    update registry address in publish scripts to 192.168.1.179:5001

--- scripts/publish-be.rb
+++ scripts/publish-be.rb
@@ -2,7 +2,7 @@
 require_relative 'base'
 include Shell
 
-REGISTRY = 'pifive:5001'
+REGISTRY = '192.168.1.179:5001'
 PROJECT_DIR = File.expand_path('..', __dir__)
 
 image = { name: 'shortener', tag: 'latest', dockerfile: '.ci/Dockerfile', context: '.' }
