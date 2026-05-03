3c9fde9d6680babffc55a51e513d7d66f706efac
alexcastrodev <alexoliveira7x@gmail.com>
2026-05-03 12:03:51 +0100

    update registry address in publish scripts to pifive:5001

--- scripts/publish-be.rb
+++ scripts/publish-be.rb
@@ -2,7 +2,7 @@
 require_relative 'base'
 include Shell
 
-REGISTRY = 'pifive:5002'
+REGISTRY = 'pifive:5001'
 PROJECT_DIR = File.expand_path('..', __dir__)
 
 image = { name: 'shortener', tag: 'latest', dockerfile: '.ci/Dockerfile', context: '.' }
--- scripts/publish-edge.rb
+++ scripts/publish-edge.rb
@@ -2,7 +2,7 @@
 require_relative 'base'
 include Shell
 
-REGISTRY = 'pifive:5002'
+REGISTRY = 'pifive:5001'
 PROJECT_DIR = File.expand_path('..', __dir__)
 
 image = { name: 'shortener-edge', tag: 'latest', dockerfile: '.ci/Dockerfile.deno', context: '.' }
